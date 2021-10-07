fbe23ed7ce9b18087a8dd9b148c64dcb06e28c8e
cantika98 <65290846+cantika98@users.noreply.github.com>
2021-10-07 20:41:08 +0700

    Update bismillah.py

--- bismillah.py
+++ bismillah.py
@@ -1,19 +1,24 @@
 import serial
-i=0
-h=0
-y=0
 
-ser = serial.Serial('/dev/ttyUSB0',9600)
+rawData = []
+imaginary = []
+real = []
+
+ser = serial.Serial('/dev/ttyUSB1',9600)
 print('Connected to Arduino')
 ser.flush()
 
 while True:
     read_serial=ser.readline().decode('utf8')
-    print(read_serial)
-#    if i < 2 : 
-#        h[i] =read_serial.split("x")
-#    else :
-#        y[i] = read_serial.split("x")
-#    
-#    print ("real: ",h[i])
-#    print ("imaginer: ",y[i])
+    for s in read_serial.splitlines():
+        for t in read_serial.split():
+                try:
+                    rawData.append(float(t))
+                except ValueError:
+                    pass
+                else:
+                    if "x" in read_serial:
+                        imaginary.append(float(read_serial.replace('x','')))
+                    else:
+                        real.append(float(read_serial))
+                        print(real)     
